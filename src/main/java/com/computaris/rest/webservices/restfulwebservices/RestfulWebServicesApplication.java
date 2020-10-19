22ca0445bc08fdab8dd9562f0459a97d8fdc4f1d
Ciurescu Raul <raul.ciurescu@computaris.com>
2020-10-19 22:30:09 +0300

    minor changes

--- src/main/java/com/computaris/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/computaris/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,6 +3,14 @@ package com.computaris.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * REpresentational State Transfer Web Services
+ * Create a User - POST /users
+ * Delete a User - DELETE /users/1
+ * Get all User - GET /users
+ * Get one User - GET /users/1
+ */
+
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 

308b642cc52e82f18c89a4016098f87f30000ec6
Ciurescu Raul <raul.ciurescu@computaris.com>
2020-10-19 23:28:41 +0300

    added User bean

---
+++ src/main/java/com/computaris/rest/webservices/restfulwebservices/User.java
@@ -0,0 +1,58 @@
+package com.computaris.rest.webservices.restfulwebservices;
+
+import java.util.Date;
+
+/**
+ * project: restful-web-services
+ * class: User
+ * created by: rci
+ * on 19/10/2020
+ */
+
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private Date birthDate;
+
+    public User(Integer id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
